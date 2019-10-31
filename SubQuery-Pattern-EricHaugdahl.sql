de100620b8e411401139e3e50c3e725220ccaa71
erichaugdahl <55147589+erichaugdahl@users.noreply.github.com>
2019-10-30 20:25:28 -0500

    Create  SubQuery-Pattern-EricHaugdahl.sql

---
+++ SubQuery-Pattern-EricHaugdahl.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT EmployeeNumber
+FROM assignment
+WHERE HoursWorked < (SELECT AVG(HoursWorked) FROM assignment)
+
+SELECT DISTINCT EmployeeNumber
+FROM assignment
+WHERE NOT EXISTS ( SELECT *
+                  FROM project
+                  WHERE NOT EXISTS ( SELECT *
+                                    FROM	assignment
+                                    WHERE Project.ProjectID=assignment.ProjectID))
